20b34b1b1bdfe2202797df02d821a4b383753a6d
Alexander Kartavenko <askemailbox@gmail.com>
2019-07-03 14:39:18 +0300

    travis

---
+++ install_mssql.sh
@@ -0,0 +1,4 @@
+sudo docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=YourStrong!Passw0rd' -p 1433:1433 -d microsoft/mssql-server-linux:2017-latest
+
+sudo apt install php-dev php-pear
+sudo pecl install pdo_sqlsrv
