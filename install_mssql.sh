c728d6ddc159818b45b12a2f120a371bac336d90
Alexander Kartavenko <askemailbox@gmail.com>
2019-07-03 15:51:27 +0300

    travis

--- install_mssql.sh
+++ install_mssql.sh
@@ -6,14 +6,14 @@ sudo docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=YourStrong!Passw0rd' -p 1433:
 sudo curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 sudo curl https://packages.microsoft.com/config/ubuntu/14.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
 sudo apt-get update
-sudo ACCEPT_EULA=Y apt-get install msodbcsql17
+sudo ACCEPT_EULA=Y apt-get -y install msodbcsql17
 # optional: for bcp and sqlcmd
-sudo ACCEPT_EULA=Y apt-get install mssql-tools
+sudo ACCEPT_EULA=Y apt-get -y install mssql-tools
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
 source ~/.bashrc
 # optional: for unixODBC development headers
-sudo apt-get install unixodbc-dev
+sudo apt-get -y install unixodbc-dev
 
 pecl install sqlsrv
 pecl install pdo_sqlsrv
